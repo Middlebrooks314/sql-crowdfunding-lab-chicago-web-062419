02a9ad5da9d154a36623589a80668a29f5858444
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-07-01 20:47:36 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,7 +11,7 @@
 # ORDER BY column_name ASC|DESC, column_name ASC|DESC;
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+
 "SELECT projects.title, SUM(pledges.amount) 
 FROM projects INNER JOIN pledges 
 ON projects.id = pledges.project_id 
@@ -20,7 +20,7 @@ ORDER BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+
 "SELECT users.name, users.age, SUM(pledges.amount)
 FROM users INNER JOIN pledges
 ON users.id = pledges.user_id
@@ -29,7 +29,7 @@ ORDER BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT projects.title, SUM(pledges.amount) - goal.pledges"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
