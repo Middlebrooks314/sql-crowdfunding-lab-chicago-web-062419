b2a93fc25e0065a271236f762f78025475f97086
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-07-01 20:25:44 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -14,7 +14,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 "Write your SQL query Here"
 "SELECT projects.title, SUM (pledges.amount)
 FROM projects JOIN pledges ON projects.id = pledges.projects_id 
-ORDER BY projects.title ASC;"
+GROUP BY projects.title ASC;"
 
 end
 
